0b53d973313ac8585271d0837f19f74625fc06a1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-06-15 11:49:42 -0700

    further revise mongoose schema

--- api/models/flashcardModel.js
+++ api/models/flashcardModel.js
@@ -2,10 +2,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CardSchema = new Schema({
-  name: {
+  cardFront: {
     type: String,
-    required: 
+    required: 'add a front of card'
+  },
+  cardBack: {
+    type: String,
+    required: 'add a back of card'
+  },
+  cardDeck: {
+    type: String,
+    required: 'add a deck to this card'
+  },
+  user: {
+    type: String
+  },
+  cardId:{
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    auto: true,
+    index: true
   }
-
-
 });
