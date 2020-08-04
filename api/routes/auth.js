e025cd6a9c30e3e1024a28ec279c8250e055375b
Marcus Jensen <marcusjensen1515@gmail.com>
2020-08-04 11:36:18 -0700

    validation middleware now working when creating a new user

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,8 +3,22 @@ const User = require('../models/userModel');
 const {registerValidation, loginValidation} = require('../../validation');
   const { create_a_user } = require('../controllers/userController');
 
-router.post('', (req,res) => {
+// router.post('', (req,res) => {
+//   create_a_user(req,res);
+// });
+
+//below is experimental
+
+router.post('', async (req,res) => {
+  const error = registerValidation(req.body);
+
+  if(error.error) return res.status(400).send(error.error.details[0].message);
+
+  const emailExists = await User.findOne({email: req.body.email});
+  if(emailExists) return res.status(400).send('this email already exists');
+
   create_a_user(req,res);
+
 });
 
 
