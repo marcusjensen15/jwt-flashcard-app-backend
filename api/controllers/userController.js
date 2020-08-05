5fbe4285e190356077cd8a943ffb1cc87d8d93e1
Marcus Jensen <marcusjensen1515@gmail.com>
2020-08-05 10:41:03 -0700

    in previous commit, migrated all password hash and salt into user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,12 +3,14 @@ const bcrypt = require('bcryptjs');
   User = mongoose.model('User');
 
  const create_a_user = async (req,res) => {
+   
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
   var new_user = new User({
     email: req.body.email,
     password: hashPassword
   });
+
   new_user.save(function(err, user){
     if (err)
       res.send(err);
